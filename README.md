4c02e78fa724311c319af488ee26c95deec6f351
jie4038 <jie4038@qq.com>
2017-05-21 14:16:11 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# project-hub
-前端项目集中管理的桌面应用，基于electron。
+# 前端项目集成工具 project-hub
+
+> 前端项目集中管理的win桌面应用，基于electron。 ---by [DH](http://denghao.me)
+
+![project-hub1.jpg][1]
+
+##### 安装步骤：
+    npm istall
+	npm start
+
+##### 2017.5.21 更新：
+- add：项目环境终止功能
+- edit：详情展示颜色区分
+- edit：项目路径选择方式
+- edit：移除多余modules
+
+##### 2017.5.20 更新：
+- add：项目添加、删除功能
+- add：项目配置（名称、地址、运行命令、打包命令）
+- add：进度详情展示
+- add：支持手动输入命令行
+
+[1]: http://denghao.me/usr/uploads/2017/05/3336752715.jpg
---
+++ src/js/renderer.js
@@ -0,0 +1,255 @@
+﻿var cp = require('child_process');
+var iconv = require('iconv-lite');
+var vm = new Vue({
+  el: "#app",
+  data: {
+    appData: {
+      list: []
+    },
+    currData: {},
+    formData: {
+      id: '',
+      title: '',
+      filePath: '',
+      cmd_dev: '',
+      cmd_build: ''
+    },
+    processData: '',
+    input_cmd: '',
+    showModify: false,
+    chirdren: {}
+  },
+  mounted() {
+    let _this = this;
+    this.getAllData();
+
+    process.on('uncaughtException', function(error) {
+      _this.displayProcess(error, 'error');
+    });
+  },
+
+  methods: {
+
+    /*
+     *获取全部数据
+     */
+    getAllData() {
+      this.appData = JSON.parse(localStorage.getItem("data")) || {
+        list: []
+      };
+    },
+
+    /*
+     *选择项目地址
+     */
+    getFilePath(e) {
+      for (let f of e.target.files) {
+        this.formData.filePath = f.path;
+      }
+    },
+
+    /*
+     *执行cmd
+     */
+    task(id, way) {
+      var _this = this;
+      var cmd = "";
+      var filePath = "";
+      var list = this.appData.list;
+      if (!id) {
+        cmd = this.input_cmd;
+      } else {
+        for (var i in list) {
+          if (id === list[i].id) {
+            cmd = "cd/d " + list[i].filePath + "&&" + list[i][way];
+          }
+        }
+      }
+
+      // eg:  'start cmd /k "cd/d f:\\project\\AI-chat&&gulp"';
+      this.chirdren = cp.exec(cmd, {
+        encoding: "binary"
+      });
+
+      _this.chirdren.stdout.on('data', (data) => {
+        console.log('ok')
+        _this.displayProcess(data);
+      })
+
+      _this.chirdren.stderr.on('data', (data) => {
+        console.log('error')
+        _this.displayProcess(data, 'error');
+      })
+
+      _this.chirdren.on('exit', (code) => {
+        _this.displayProcess("======= done! =======", 'done');
+      });
+
+    },
+
+    // 关闭进程
+    close(id) {
+      var _this = this;
+      console.log(_this.chirdren.pid);
+
+      if (!_this.chirdren.pid) {
+        _this.displayProcess('sorry! process pid missed.', 'error');
+        return;
+      }
+
+      let cmd='taskkill /PID '+_this.chirdren.pid+' /T /F';
+
+      cp.exec(cmd, function(error, stdout, stderr) {
+        if (stdout) _this.displayProcess("[process closed!]", 'done');
+        if (stderr) _this.displayProcess(stderr, 'error');
+        if (error) _this.displayProcess(error, 'error');
+      });
+
+    },
+
+    // 展示结果
+    displayProcess(str, type) {
+      this.scrollToBottom();
+      let s=str.toString();
+      switch (type) {
+        case 'error':
+          this.processData += '<div class="c-red">' + this.gbk(s) + '</div>';
+          break;
+        case 'done':
+          this.processData += '<div class="c-green">' + this.gbk(s) + '</div>';
+          break;
+        default:
+          this.processData += this.gbk(s) + '<br>';
+          break;
+      }
+    },
+
+    // 删除item
+    del(id) {
+      if (!id) return;
+      var list = this.appData.list;
+      for (var i in list) {
+        if (id === list[i].id) {
+          list.splice(i, 1);
+        }
+      }
+      localStorage.setItem("data", JSON.stringify(this.appData));
+    },
+
+
+    /*
+     *打开modify弹窗
+     */
+    openModify(id) {
+      if (id) {
+        var list = this.appData.list;
+        for (var i in list) {
+          if (id === list[i].id) {
+            this.currData = list[i];
+            this.formData = this.copy(this.currData);
+          }
+        }
+      } else {
+        this.formData = {}
+      }
+      this.toggleShow();
+    },
+
+    /*
+     *保存modify
+     */
+    saveModify(id) {
+
+      if (!this.formData.title) {
+        this.toast("项目名称必填！");
+        return;
+      }
+
+      var list = this.appData.list;
+      if (id) {
+        for (var i in list) {
+          if (id === list[i].id) {
+            list[i] = this.formData;
+          }
+        }
+      } else {
+        this.formData.id = Date.parse(new Date()) / 1000;
+        list.push(this.formData);
+      }
+
+      this.currData = this.formData;
+      this.toggleShow();
+      localStorage.setItem("data", JSON.stringify(this.appData));
+    },
+
+    toggleShow() {
+      this.showModify = !this.showModify;
+    },
+
+
+    /*
+     *克隆对象
+     */
+    copy(obj) {
+      var r = {};
+      for (var key in obj) {
+        r[key] = typeof obj[key] === 'object' ? this.copy(obj[key]) : obj[key];
+      }
+      return r;
+    },
+
+    /*
+     *取消modify
+     */
+    cancelModify() {
+      this.toggleShow();
+    },
+
+    /*
+     *中文乱码问题
+     */
+    gbk(str) {
+      return iconv.decode(new Buffer(str, 'binary'), 'gbk');
+    },
+
+    /*
+     *保持底部可见
+     */
+    scrollToBottom() {
+      var b = document.getElementById('bottomLine');
+      b.scrollIntoView();
+    },
+
+    /*
+     * Toast 提示框
+     */
+    toast(msg, callback, time) {
+      if (!document.getElementById('js-toast')) {
+        var box = document.createElement("div"),
+          span = document.createElement("span"),
+          txt = document.createTextNode(msg);
+        box.id = "js-toast";
+        span.id = "js-toast-msg";
+        box.className = "js-toast";
+        span.appendChild(txt);
+        box.appendChild(span);
+        document.body.appendChild(box);
+      }
+
+      if (box.addEventListener) {
+        box.addEventListener("touchmove", function(e) {
+          e.preventDefault();
+        })
+      } else {
+        box.attachEvent("ontouchmove", function(e) {
+          window.event.returnValue = false;
+        })
+      }
+      setTimeout(function() {
+        document.body.removeChild(box);
+        typeof callback === "function" && callback();
+      }, (time ? time : 1500));
+    }
+
+  }
+});
